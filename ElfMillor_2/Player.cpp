e1c7160649aef5c108baf30acb09e4b33ec80ef9
keishiF <suzaku7465@gmail.com>
2025-02-07 09:42:51 +0900

    up

--- ElfMillor_2/Player.cpp
+++ ElfMillor_2/Player.cpp
@@ -23,7 +23,7 @@ namespace
 	constexpr int kRightEndWidth = 1120;
 
 	// プレイヤーの初期HP
-	constexpr int kDefaultHp = 5;
+	constexpr int kDefaultHp = 10;
 
 	// プレイヤーの移動速度
 	constexpr float kSpeed = 5.0f;
